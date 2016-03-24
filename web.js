ef1a812902ddc7b54f9cd19ec3559ded7492ced0
Lakshmi S Nair <laxmi256@users.noreply.github.com>
2016-03-24 18:19:24 +0530

    Update web.js

--- web.js
+++ web.js
@@ -698,29 +698,6 @@ app.get('/user/:user_id/json', function (request, response) {
     });
 });
 
-app.post('/shoppinglist/:shoppinglist_id/delete/json', function (request, response) {
-    "use strict";
-    pool.getConnection(function (err, connection) {
-        if (err) {
-            console.log('error : ', err);
-            throw err;
-        }       
-        var shoppinglist_id = request.params.shoppinglist_id;
-        var sql = "delete from shoppinglist where id=" + shoppinglist_id + ";";
-        connection.query(sql, function (err) {
-            if (err) {
-                console.log('error : ', err);
-                connection.release();
-                throw err;
-            }
-            connection.commit();
-            sleep(1000);
-            connection.release();
-            response.send(200);
-        });
-    });
-});
-
 
 app.delete('/user/:user_id/json', function (request, response) {
     "use strict";
@@ -788,6 +765,30 @@ app.get('/shoppinglist/:shoppinglist_id/json', function (request, response) {
             sleep(1000);
             connection.release();
             response.send(rows);
+        });
+    });
+});
+
+
+app.post('/shoppinglist/:shoppinglist_id/delete/json', function (request, response) {
+    "use strict";
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            console.log('error : ', err);
+            throw err;
+        }       
+        var shoppinglist_id = request.params.shoppinglist_id;
+        var sql = "delete from shoppinglist where id=" + shoppinglist_id + ";";
+        connection.query(sql, function (err) {
+            if (err) {
+                console.log('error : ', err);
+                connection.release();
+                throw err;
+            }
+            connection.commit();
+            sleep(1000);
+            connection.release();
+            response.send(200);
         });
     });
 });
