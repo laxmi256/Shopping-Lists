b6275a2061e73ae57040cd3ba52bad05ecd836bc
Lakshmi S Nair <laxmi256@users.noreply.github.com>
2016-03-30 22:42:17 +0530

    Update web.js

--- web.js
+++ web.js
@@ -1509,16 +1509,14 @@ app.put('/user/:user_id/edit1/json', function (request, response) {
             console.log('error : ', err);
             throw err;
         }        
-        /*var name = request.body.name;
+        var name = request.body.name;
         var email = request.body.email;
         var picture = request.body.picture;
         var provider = request.body.provider;
-        
-        resonse.send(name + ', ' + email + ', ' + picture + ', ' + provider);*/
-        
         sleep(1000);
         connection.release();
-        response.send("HELLO");
+        //response.send("HELLO");
+        resonse.send(name + ', ' + email + ', ' + picture + ', ' + provider);
     });
 });
 
